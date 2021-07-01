bd0b45301fc130c879095f45a27a3090796521b5
YuetongXU <48643820+YuetongXU@users.noreply.github.com>
2021-07-01 22:33:24 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cropgbm',
+    version='1.1.1',
+    description=(
+        'Crop Genomic Breeding machine (CropGBM) is a multifunctional program that integrates data preprocessing, population structure analysis, SNP selection, phenotype prediction, and data visualization.'
+    ),
+    long_description=open('README.md').read(),
+    author='Yuetong Xu, Jun Yan',
+    author_email='cau_cab_xu@163.com',
+    maintainer='Yuetong Xu',
+    maintainer_email='cau_cab_xu@163.com',
+    license='MIT License',
+    platforms=["all"],
+    url='https://github.com/YuetongXU/CropGBM',
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'cropgbm = cropgbm.kernel:main',
+        ]
+    },
+    package_data={
+        'cropgbm': ['configfile.params']
+    },
+    classifiers=[
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3',
+    install_requires=[
+        'wheel',
+	'numpy',
+	'scipy',
+	'pandas',
+	'scikit-learn>=0.24',
+	'lightgbm',
+	'matplotlib',
+	'seaborn'
+    ]
+
+)
